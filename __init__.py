b0c1e655f37386997af92f4cae17f5ffbced2e73
Venceslas Roullier <venceslas.roullier@opendatasoft.com>
2021-06-09 17:52:24 +0200

    plugi works on all geometries, nearly all field types, and is entirely in the script file

--- __init__.py
+++ __init__.py
@@ -35,51 +35,11 @@ class MinimalPlugin:
         QMessageBox.information(None, 'ODS plugin',
                                 "When you press the button, you'll get a new dataset from scratch.")
 
-        # Input : name of the column that contains the geometry (either geo_point_2d or geo_shape)
-        geom_data_name = "geo_shape"
+        # TODO : add input
+        # TODO : deal with conversion from type to qgsField
+        plugin_input = {}
+        plugin_input["geom_data_name"] = "sens_velo"
+        plugin_input["domain"] = "vroullier"
+        plugin_input["dataset_id"] = "reseau-des-itineraires-cyclables"
 
-        metadata = pyqgis_script.import_dataset_metadata("vroullier", "provinces-and-territories-canada")
-        dataset = pyqgis_script.import_dataset("vroullier", "provinces-and-territories-canada")
-
-        for column in metadata["results"][0]["fields"]:
-            if column["name"] == geom_data_name:
-                geom_data_type = column["type"]
-            # TODO check if geo_shape or geo_point_2d
-
-        attribute_list = pyqgis_script.create_attributes(metadata, geom_data_type)
-
-        if geom_data_type == "geo_point_2d":
-            pyqgis_script.create_layer_old("Point", metadata, geom_data_type, dataset, geom_data_name,
-                                           attribute_list)
-        elif geom_data_type == "geo_shape":
-            geometry_set = set()
-            layer_dict = {}
-            for line in range(dataset["total_count"]):
-                json_geometry = dataset["results"][line][geom_data_name]["geometry"]["type"]
-                json_geometry = pyqgis_script.geom_to_multi_geom(json_geometry)
-                # TODO : check if json_geometry in accepted geometries, namely MultiPolygon, MultiLineString, MultiPoint
-                if json_geometry not in geometry_set:
-                    geometry_set.add(json_geometry)
-                    layer = pyqgis_script.create_layer(json_geometry, attribute_list)
-                    layer_dict[json_geometry] = layer
-                    # TODO : else : not an accepted geojson geom, exception
-                pyqgis_script.fill_layer(layer_dict[json_geometry], metadata, dataset, geom_data_type, geom_data_name,
-                                         line)
-
-            features = layer_dict["MultiPolygon"].getFeatures()
-            for feat in features:
-                print("F:", feat.id(), feat.attributes(), feat.geometry())
-
-            for layer in layer_dict.values():
-                layer.updateExtents()
-
-            features = layer_dict["MultiPolygon"].getFeatures()
-            for feat in features:
-                print("F:", feat.id(), feat.attributes(), feat.geometry())
-
-            from qgis.core import QgsProject
-            from qgis.gui import QgsMapCanvas
-            QgsProject.instance().addMapLayer(layer_dict["MultiPolygon"])
-            canvas = QgsMapCanvas()
-            canvas.setExtent(layer_dict["MultiPolygon"].extent())
-            canvas.setLayers([layer_dict["MultiPolygon"]])
+        pyqgis_script.import_to_qgis(plugin_input)
--- pyqgis_script.py
+++ pyqgis_script.py
@@ -9,65 +9,9 @@ from qgis.PyQt.QtCore import QVariant
 V2_API_CHUNK_SIZE = 100
 JSON_TO_QGIS_TYPES = {"text": QVariant.String, "double": QVariant.Double, "int": QVariant.Int,
                       "boolean": QVariant.Bool, "date": QVariant.Date, "datetime": QVariant.DateTime,
-                      "geo_point_2d": QVariant.String}
-
-
-def create_layer_from_dataset():
-    """This script modifies the dataset it uses."""
-
-    # Don't forget to adapt the path to your computer for now
-    path_to_festivals = "/Users/venceslas/qgis-ods-plugin/festivals-du-finistere.geojson"
-
-    festival_vlayer = QgsVectorLayer(path_to_festivals, "Festivals du Finistère", "ogr")
-
-    if not festival_vlayer.isValid():
-        print("Layer failed to load!")
-    else:
-        QgsProject.instance().addMapLayer(festival_vlayer)
-
-    feat = QgsFeature(festival_vlayer.fields())
-    feat.setAttributes(
-        ["29", "2020-05-27", "CE197", "www.brestivaldelachaussette.com", "Annuelle", "PLOUZANE", "Finist\u00e8re", 29.0,
-         "BRESTIVAL DE LA CHAUSSETTE", (48.4, -4.6), "2020-05-25", 0.0, 20.0, 21.0, "29280", 5.0,
-         "C'est LE festival de la chaussette : amoureux des pieds bien traités, amis du tricot ou encore givrés du "
-         "panard, vous y trouverez les dernières nouveautés en matière de chaussette bretonne...",
-         "Chaussettes, what else ?", "29280", "Bretagne", "Transdisciplinaire", "1998-01-01", "05 (mai)", "PLOUZANE"])
-    feat.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(-4.6, 48.4)))
-    (res, outFeats) = festival_vlayer.dataProvider().addFeatures([feat])
-    if iface.mapCanvas().isCachingEnabled():
-        festival_vlayer.triggerRepaint()
-    else:
-        iface.mapCanvas().refresh()
-
-
-def create_layer_with_memory_provider():
-    """The script dataset is temporary, and will be deleted when you quit QGIS."""
-
-    vlayer = QgsVectorLayer("Point", "temporary_futurama_points", "memory")
-    provider = vlayer.dataProvider()
-    provider.addAttributes([QgsField("name", QVariant.String),
-                            QgsField("age", QVariant.Int),
-                            QgsField("size", QVariant.Double),
-                            QgsField("is_human", QVariant.Bool),
-                            QgsField("date_of_birth", QVariant.Date)])
-
-    vlayer.updateFields()
-
-    feat = QgsFeature()
-    feat.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(10, 10)))
-    feat.setAttributes(["Fry", 30, 1.8, True, "1975-01-21"])
-    provider.addFeatures([feat])
-    feat.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(-10, 10)))
-    feat.setAttributes(["Farnsworth", 200, 1.2, True, "1990-05-29"])
-    provider.addFeatures([feat])
-    feat.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(10, -10)))
-    feat.setAttributes(["Zoidberg", 50, 1.6, False])
-    provider.addFeatures([feat])
-    feat.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(-10, -10)))
-    feat.setAttributes(["Bender", 20, 1.8, False, "2100-05-05"])
-    provider.addFeatures([feat])
-
-    vlayer.updateExtents()
+                      "geo_point_2d": QVariant.List}
+ACCEPTED_GEOMETRY = {"MultiPoint", "MultiLineString", "MultiPolygon"}
+ACCEPTED_TYPE = {"geo_point2d", "geo_shape"}
 
 
 def import_dataset(domain_url, dataset_id):
@@ -78,14 +22,14 @@ def import_dataset(domain_url, dataset_id):
     json_dataset = first_query.json()
     total_count = json_dataset['total_count']
     offset = V2_API_CHUNK_SIZE
-    while offset <= total_count:
+    while offset <= total_count and offset < 200:
         query = requests.get(
             "https://{}.opendatasoft.com/api/v2/catalog/datasets/{}/query?limit={}&offset={}".format(domain_url,
                                                                                                      dataset_id,
                                                                                                      V2_API_CHUNK_SIZE,
                                                                                                      offset))
         json_dataset['results'] += query.json()['results']
-        offset += 100
+        offset += V2_API_CHUNK_SIZE
     return json_dataset
 
 
@@ -112,84 +56,13 @@ def import_dataset_metadata(domain_url, dataset_id):
     return query.json()
 
 
-def create_layer_old(json_geometry, metadata, geom_data_type, dataset, geom_data_name, attribute_list):
-    from qgis.core import (QgsVectorLayer, QgsFeature, QgsGeometry, QgsPointXY, QgsProject)
-    from qgis.gui import QgsMapCanvas
-
-    vlayer = QgsVectorLayer(json_geometry, "temporary_dataset_points", "memory")
-    vlayer.dataProvider().setEncoding("UTF-8")
-    provider = vlayer.dataProvider()
-    provider.addAttributes(attribute_list)
-    vlayer.updateFields()
-
-    magic_length = len(dataset['results'])
-    for i in range(magic_length):
-        feature = QgsFeature()
-        coordinates = dataset['results'][i][geom_data_name]
-        # if geometry is Point, else adapt with right geometry e.g. QgsMultiPolygonXY
-        if geom_data_type == "geo_point_2d":
-            feature.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(coordinates['lon'], coordinates['lat'])))
-        else:
-            print("no geom for you")
-        feature_values = []
-        for field in metadata["results"][0]["fields"]:
-            if field["type"] != geom_data_type:  # if geom attribute is geo_point_2d, else adapt with geo_shape
-                feature_values.append(dataset['results'][i][field['name']])
-
-        feature.setAttributes(feature_values)
-        provider.addFeatures([feature])
-
-    vlayer.updateExtents()
-    QgsProject.instance().addMapLayer(vlayer)
-    canvas = QgsMapCanvas()
-    canvas.setExtent(vlayer.extent())
-    canvas.setLayers([vlayer])
-
-
-def create_layer(json_geometry, attribute_list):
-    from qgis.core import QgsVectorLayer
-    layer = QgsVectorLayer(json_geometry, "temporary_dataset_{}".format(json_geometry), "memory")
-    layer.dataProvider().setEncoding("UTF-8")
-    provider = layer.dataProvider()
-    provider.addAttributes(attribute_list)
-    layer.updateFields()
-    return layer
-
-
-def fill_layer(layer, metadata, dataset, geom_data_type, geom_data_name, line):
-    from qgis.core import (QgsFeature, QgsGeometry, QgsPointXY, QgsMultiPolygon, QgsMultiLineString,
-                           QgsMultiPoint, QgsPoint)
-    from osgeo import ogr
-    import json
-
-    provider = layer.dataProvider()
-    feature = QgsFeature()
-    geometry = dataset['results'][line][geom_data_name]["geometry"]
-
-    if geom_data_type == "geo_point_2d":
-        feature.setGeometry(
-            QgsGeometry.fromPointXY(QgsPointXY(geometry["coordinates"]['lon'], geometry["coordinates"]['lat'])))
-    else:
-        geometry_string = json.dumps(geometry)
-        geom = ogr.CreateGeometryFromJson(geometry_string)
-        feature.setGeometry(QgsGeometry.fromWkt(geom.ExportToWkt()))
-    feature_values = []
-    for field in metadata["results"][0]["fields"]:
-        if field["type"] != geom_data_type and field["type"] != "geo_point_2d":
-            feature_values.append(dataset['results'][line][field['name']])
-
-    feature.setAttributes(feature_values)
-    print(feature.attributes(), feature.geometry())
-    provider.addFeatures([feature])
-
-
 def create_attributes(metadata, geom_data_type):
     from qgis.PyQt.QtCore import QVariant
     from qgis.core import QgsField
 
     attribute_list = []
     for field in metadata["results"][0]["fields"]:
-        if field["type"] != geom_data_type and field["type"] != "geo_point_2d":
+        if field["type"] != geom_data_type:
             if "multivalued" in field["annotations"]:
                 if field["type"] == "text":
                     attribute_list.append(QgsField(field["name"], QVariant.StringList))
@@ -204,3 +77,105 @@ def geom_to_multi_geom(geometry):
     if "Multi" not in geometry:
         geometry = "Multi" + geometry
     return geometry
+
+
+def create_layer(json_geometry, attribute_list):
+    from qgis.core import QgsVectorLayer
+    layer = QgsVectorLayer(json_geometry, "temporary_dataset_{}".format(json_geometry), "memory")
+    layer.dataProvider().setEncoding("UTF-8")
+    provider = layer.dataProvider()
+    provider.addAttributes(attribute_list)
+    layer.updateFields()
+    return layer
+
+
+def create_feature(metadata, dataset, geom_data_type, geom_data_name, line):
+    from qgis.core import (QgsFeature, QgsGeometry, QgsPointXY, QgsMultiPolygon, QgsMultiLineString,
+                           QgsMultiPoint, QgsPoint)
+    from osgeo import ogr
+    import json
+
+    feature = QgsFeature()
+
+    if geom_data_type == "geo_point_2d":
+        geometry = dataset['results'][line][geom_data_name]
+        feature.setGeometry(
+            QgsGeometry.fromPointXY(QgsPointXY(geometry['lon'], geometry['lat'])))
+    else:
+        geometry = dataset['results'][line][geom_data_name]["geometry"]
+        geometry_string = json.dumps(geometry)
+        geom = ogr.CreateGeometryFromJson(geometry_string)
+        feature.setGeometry(QgsGeometry.fromWkt(geom.ExportToWkt()))
+    feature_values = []
+
+    for field in metadata["results"][0]["fields"]:
+        if field["type"] != geom_data_type:
+            if field["type"] == "geo_point_2d":
+                feature_values.append(list(dataset['results'][line][field['name']].values()))
+            else:
+                feature_values.append(dataset['results'][line][field['name']])
+
+    feature.setAttributes(feature_values)
+    return feature
+
+
+def import_to_qgis(plugin_input):
+    metadata = import_dataset_metadata(plugin_input["domain"], plugin_input["dataset_id"])
+    dataset = import_dataset(plugin_input["domain"], plugin_input["dataset_id"])
+    geom_data_name = plugin_input["geom_data_name"]
+
+    geom_data_type = ""
+    for column in metadata["results"][0]["fields"]:
+        if column["name"] == geom_data_name:
+            geom_data_type = column["type"]
+    # TODO proper exception
+    if geom_data_type not in ACCEPTED_TYPE:
+        print("bad column")
+        """from qgis.core import (Qgis, QgsMessageLog)
+        QgsMessageLog.logMessage("Your plugin code has crashed!", level=Qgis.Critical)"""
+
+    attribute_list = create_attributes(metadata, geom_data_type)
+    layer_dict = {}
+    if geom_data_type == "geo_point_2d":
+        layer = create_layer("Point", attribute_list)
+        layer_dict["Point"] = layer
+        for line in range(dataset["total_count"]):
+            layer_dict["Point"].startEditing()
+            feature = create_feature(metadata, dataset, geom_data_type, geom_data_name, line)
+            layer_dict["Point"].addFeatures([feature])
+            layer_dict["Point"].commitChanges()
+    elif geom_data_type == "geo_shape":
+        geometry_set = set()
+        for line in range(len(dataset["results"])):
+            if dataset["results"][line][geom_data_name] is not None:
+                json_geometry = dataset["results"][line][geom_data_name]["geometry"]["type"]
+                json_geometry = geom_to_multi_geom(json_geometry)
+                if json_geometry in ACCEPTED_GEOMETRY:
+                    if json_geometry not in geometry_set:
+                        geometry_set.add(json_geometry)
+                        layer = create_layer(json_geometry, attribute_list)
+                        layer_dict[json_geometry] = layer
+                    layer_dict[json_geometry].startEditing()
+                    feature = create_feature(metadata, dataset, geom_data_type, geom_data_name, line)
+                    layer_dict[json_geometry].addFeatures([feature])
+                    layer_dict[json_geometry].commitChanges()
+                else:
+                    # TODO : have a proper exception
+                    print("exception")
+
+    for layer in layer_dict.values():
+        layer.updateExtents()
+
+    from qgis.core import QgsProject
+    from qgis.gui import QgsMapCanvas
+    for layer in layer_dict.values():
+        QgsProject.instance().addMapLayer(layer)
+    canvas = QgsMapCanvas()
+    for layer in layer_dict.values():
+        canvas.setExtent(layer.extent())
+        canvas.setLayers([layer])
+
+
+"""provider = layer_dict[json_geometry].dataProvider()
+                feature = pyqgis_script.create_feature(metadata, dataset, geom_data_type, geom_data_name, line)
+                provider.addFeatures([feature])"""
